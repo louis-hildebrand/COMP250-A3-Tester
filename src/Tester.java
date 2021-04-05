6332f8918219a756fd44c01f6d399ec992c920c7
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-04-05 15:35:26 -0400

    Remove reference to RuntimeTester

--- src/Tester.java
+++ src/Tester.java
@@ -1043,11 +1043,7 @@ public class Tester {
 			}
 		}
 		if (numPassed == tests.length) {
-			System.out.println(
-					"All clear!\n\nDon't forget to check the time complexity of your methods."
-							+ "\nSasha\'s RuntimeTester is a great tool for this:"
-							+ "\nhttps://www.youtube.com/watch?v=UNCopqdTX00"
-							+ "\n\nYou can find some basic tests (using the RuntimeTester) in TimeTester.java");
+			System.out.println("All clear! Great work :)");
 		}
 	}
 }
