f2fac5b3c75ee74c6ef03f8e271242aa7d59136e
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-04-09 14:00:30 -0400

    Clean up imports

--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
